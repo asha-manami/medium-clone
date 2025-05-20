7bbe9e1e4746a53e19ef411b492aa6a1c04db085
asha-manami <ashamanami@gmail.com>
2025-05-20 12:48:16 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <h1 className="font-serif text-3xl font-bold">NextJs Clerk Starter</h1>
         <p className="mt-3 text-muted-foreground">
           A NextJs starter template with Clerk authentication, shadcn/ui and
-          Tailwind CSS.
+          Tailwind CSS. Test
         </p>
       </div>
     </section>
